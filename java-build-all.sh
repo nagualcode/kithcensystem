264648c21d7bafdb6fb315193985b3cffd375109
Fred <nagual@nagual.local>
2024-09-17 15:04:10 -0300

    auto

--- java-build-all.sh
+++ java-build-all.sh
@@ -4,9 +4,10 @@ for dir in *; do
 	if cd "$dir" 2>/dev/null; then
 		if ls pom.xml 2>/dev/null; then
 			pwd
-			mvn clean install
+			if ! mvn clean install; then echo "error"; exit 1; fi
 			ls target
 		fi
 		cd ..
 	fi
 done
+find . -name *.jar -type f -exec stat --format='%n %w' {} \; 
